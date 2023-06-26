8b44f10f5615a1558e8ba89da1798d5475e2fbc3
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-06-26 12:17:32 -0400

    complete spacing of game icon for field

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 const prompt = require('prompt-sync')();
 
 // GAME PIECE VARIABLES
-const hat = '\u{26D1}\u{FE0F}';
-const hole = '\u{26AB}';
-const fieldChar = '\u{26D3}';
+const hat = '\u{26D1}\u{FE0F} '.trimStart().padEnd(3);
+const hole = '\u{26AB}'.trimStart();
+const fieldChar = '\u{26D3} '.padEnd(2);
 const pathChar = '\u{2728}';
-
 
 class gameField {
     constructor(fieldArray) {
         this.fieldArray = fieldArray;
     }
+
+    print() {
+        const displayField = this.fieldArray.map(row => {
+            return row.join('');
+        }).join('\n');
+        console.log(displayField);
+    }
 }
 
 // TESTING CODE
-console.log("HAT: " + hat);
-console.log("HOLE: " + hole);
-console.log("FIELDCHAR: " + fieldChar);
-console.log("PATHCHAR: " + pathChar);
+const myField = new gameField([
+    [pathChar, fieldChar, hole, fieldChar, fieldChar, fieldChar],
+    [fieldChar, hat, fieldChar, hole, fieldChar, fieldChar],
+    [fieldChar, fieldChar, fieldChar, fieldChar, fieldChar, fieldChar],
+    [hat, fieldChar, fieldChar, hole, fieldChar, fieldChar],
+    [fieldChar, fieldChar, hole, fieldChar, hole, fieldChar]
+]);
+
+console.log(myField);
+myField.print();
