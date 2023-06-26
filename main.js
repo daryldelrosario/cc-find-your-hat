b95aaa4a5e84faa6d33b45f51d8917f1a81f8ede
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-06-26 10:43:32 -0400

    complete choice for game pieces

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const prompt = require('prompt-sync')();
+
+// GAME PIECE VARIABLES
+const hat = '\u{26D1}\u{FE0F}';
+const hole = '\u{26AB}';
+const fieldChar = '\u{26D3}';
+const pathChar = '\u{2728}';
+
+
+class gameField {
+    constructor(fieldArray) {
+        this.fieldArray = fieldArray;
+    }
+}
+
+// TESTING CODE
+console.log("HAT: " + hat);
+console.log("HOLE: " + hole);
+console.log("FIELDCHAR: " + fieldChar);
+console.log("PATHCHAR: " + pathChar);
